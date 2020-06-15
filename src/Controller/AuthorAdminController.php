8fddf2c2aa5c92d4a767c45ede6c5d173d8045d5
Dawid Szulc <niceadres@gmail.com>
2020-06-15 21:38:27 +0200

    Create new controller

---
+++ src/Controller/AuthorAdminController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Author;
+use App\Form\AuthorType;
+use App\Repository\AuthorRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuthorAdminController extends AbstractController
+{
+    /**
+     * @var Request
+     */
+    private $request;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * AuthorAdminController constructor.
+     * @param RequestStack $request
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(RequestStack $request, EntityManagerInterface $entityManager)
+    {
+        $this->request = $request->getCurrentRequest();
+        $this->entityManager = $entityManager;
+    }
+
+
+    /**
+     * List of all authors
+     *
+     * @Route("admin/author", methods="GET", name="admin_author_index")
+     * @param AuthorRepository $authorRepository
+     * @return Response
+     */
+    public function index(AuthorRepository $authorRepository): Response
+    {
+        $authors = $authorRepository->findAll();
+
+        return $this->render('author_admin/index.html.twig', [
+            'authors' => $authors,
+        ]);
+    }
+
+    /**
+     * New author
+     *
+     * @Route("admin/author/new", methods="GET|POST", name="admin_author_new")
+     */
+    public function new(): Response
+    {
+        $form = $this->createForm(AuthorType::class);
+        $form->handleRequest($this->request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var Author $data */
+            $data = $form->getData();
+
+            $this->entityManager->persist($data);
+            $this->entityManager->flush();
+
+            $this->addFlash('success', sprintf("Pomyślnie dodano rekord o ID: %d", $data->getId()));
+
+            return $this->redirectToRoute('admin_author_index');
+        }
+
+        return $this->render('author_admin/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * Edit author
+     *
+     * @Route("admin/author/{id<\d+>}/edit", methods="GET|POST", name="admin_author_edit")
+     * @param Author $author
+     * @return Response
+     */
+    public function edit(Author $author): Response
+    {
+        $form = $this->createForm(AuthorType::class, $author);
+        $form->handleRequest($this->request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->flush();
+
+            $this->addFlash('success', sprintf("Pomyślnie zmieniono rekord o ID: %d", $author->getId()));
+
+            return $this->redirectToRoute('admin_author_index');
+        }
+
+        return $this->render('author_admin/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Controller/CategoryAdminController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryAdminController extends AbstractController
+{
+    /**
+     * @var Request
+     */
+    private $request;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * CategoryAdminController constructor.
+     * @param RequestStack $request
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(RequestStack $request, EntityManagerInterface $entityManager)
+    {
+        $this->request = $request->getCurrentRequest();
+        $this->entityManager = $entityManager;
+    }
+
+
+    /**
+     * List of all categories
+     *
+     * @Route("/admin/category", methods="GET", name="admin_category_index")
+     * @param CategoryRepository $categoryRepository
+     * @return Response
+     */
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        $categories = $categoryRepository->findAll();
+
+        return $this->render('category_admin/index.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
+
+    /**
+     * New category
+     *
+     * @Route("admin/category/new", methods="GET|POST", name="admin_category_new")
+     */
+    public function new()
+    {
+        $form = $this->createForm(CategoryType::class);
+        $form->handleRequest($this->request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            /** @var Category $data */
+            $data = $form->getData();
+
+            $this->entityManager->persist($data);
+            $this->entityManager->flush();
+
+            $this->addFlash('success', sprintf("Pomyślnie dodano rekord o ID: %d", $data->getId()));
+
+            return $this->redirectToRoute('admin_category_index');
+        }
+
+        return $this->render('category_admin/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * Edit category
+     *
+     * @Route("admin/category/{id<\d+>}/edit", methods="GET|POST", name="admin_category_edit")
+     * @param Category $category
+     * @return RedirectResponse|Response
+     */
+    public function edit(Category $category)
+    {
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($this->request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->flush();
+
+            $this->addFlash('success', sprintf("Pomyślnie zmieniono rekord o ID: %d", $category->getId()));
+
+            return $this->redirectToRoute('admin_category_index');
+        }
+
+        return $this->render('category_admin/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
